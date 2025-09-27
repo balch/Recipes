17547fe403d5af75beee337087c82e0bcd68f228
balch <balch61@gmail.com>
2025-09-27 08:38:53 -0700

    update InfoScreen URL to include deepThoughts parameter

--- app/src/main/java/org/balch/recipes/features/info/InfoScreen.kt
+++ app/src/main/java/org/balch/recipes/features/info/InfoScreen.kt
@@ -15,6 +15,6 @@ fun InfoScreen(
     val theme = if (isSystemInDarkTheme()) "dark" else "light"
     WebViewScreen(
         modifier = Modifier.safeDrawingPadding(),
-        url = "https://jay-balcher-resume-792247382464.us-west1.run.app/?theme=$theme&topbar=false"
+        url = "https://jay-balcher-resume-792247382464.us-west1.run.app/?theme=$theme&topbar=false&deepThoughts=true"
     )
 }
