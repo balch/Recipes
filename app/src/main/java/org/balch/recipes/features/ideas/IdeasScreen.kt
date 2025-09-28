814cb7b2ee06c4f8cee501936646d30985a6646e
balch <balch61@gmail.com>
2025-09-28 15:19:29 -0700

    remove `rememberSaveable` from `IdeasScreen` and replace with `remember` for `GridItem` state management

--- app/src/main/java/org/balch/recipes/features/ideas/IdeasScreen.kt
+++ app/src/main/java/org/balch/recipes/features/ideas/IdeasScreen.kt
@@ -41,7 +41,6 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -747,7 +746,7 @@ private fun <T> rememberRecipeItems(
     regularItems: List<T>,
     codeRecipes: List<CodeRecipe>,
     itemWrapper: (T) -> GridItem
-): List<GridItem> = rememberSaveable {
+): List<GridItem> = remember {
     if (codeRecipes.isEmpty()) {
         regularItems.map(itemWrapper)
     } else {
