6249a3ba82cc76e2fc9fc173f94cdea4559ab44e
balch <balch61@gmail.com>
2025-10-07 21:55:01 -0700

    refactor: Use `@param` annotation for better context injection and add ignored parcel property for `Area` model

--- app/src/main/java/org/balch/recipes/core/models/Area.kt
+++ app/src/main/java/org/balch/recipes/core/models/Area.kt
@@ -1,6 +1,7 @@
 package org.balch.recipes.core.models
 
 import android.os.Parcelable
+import kotlinx.parcelize.IgnoredOnParcel
 import kotlinx.parcelize.Parcelize
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
@@ -14,6 +15,7 @@ data class Area(
     @SerialName("strArea")
     val name: String
 ): UniqueItem, Parcelable {
+    @IgnoredOnParcel
     override val id = name
 }
 
